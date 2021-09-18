708ba3a823f61353782da98958c654b0fb81939c
2ujin <suddenlydisappear200@gmail.com>
2021-09-19 03:20:52 +0900

    feat: 영역구분 완료

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,22 @@
+import styled from "styled-components";
 import Profile from "../Profile/Profile";
 import Repo from "../Repo/Repo";
 import Top from "../Top/Top";
+
+const MainWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
 
 function Home() {
   return (
     <>
       <Top />
-      <Profile />
-      <Repo />
+      <MainWrapper>
+        <Profile />
+        <Repo />
+      </MainWrapper>
     </>
   );
 }
