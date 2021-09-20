2523ebe09f7c3bf55f8b2bcf97eec64093f1e304
2ujin <suddenlydisappear200@gmail.com>
2021-09-20 21:43:39 +0900

    feat: 저장소 헤더 부분

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,12 +1,31 @@
 import styled from "styled-components";
+import Tap from "./Tap";
 
-const RepoWrapper = styled.div`
+const Wrapper = styled.div`
   flex: 4;
-  background-color: #ffc964;
+  padding: 10px 50px;
+`;
+
+const RepoTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  align-items: center;
+  h2 {
+    font-size: 20px;
+  }
 `;
 
 function Repo() {
-  return <RepoWrapper>저장소 영역</RepoWrapper>;
+  return (
+    <Wrapper>
+      <Tap />
+      <RepoTop>
+        <h2>Pinned</h2>
+        <span>Customize your pins</span>
+      </RepoTop>
+    </Wrapper>
+  );
 }
 
 export default Repo;
