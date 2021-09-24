6b41298f59f3ffd2d97aea6a91508741b3126fd8
2ujin <suddenlydisappear200@gmail.com>
2021-09-24 16:50:36 +0900

    fix

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -33,7 +33,7 @@ function Home() {
       try {
         // .env 파일에 관리해야합니다
         const octokit = new Octokit({
-          auth: process.env.TOKEN,
+          auth: "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",
         });
 
         const _user: any = await octokit.request("GET /users/{username}", {
